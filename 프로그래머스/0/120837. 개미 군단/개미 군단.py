4c7a9d254f5f4e41a924044f19caff0d6757a292
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-10-07 16:38:44 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,10 +1,2 @@
 def solution(hp):
-    answer = 0
-    while (hp > 2):
-        if hp >= 5:
-            answer += hp // 5
-            hp %= 5
-        elif hp >= 3:
-            answer += 1
-            hp %= 3
-    return answer + hp
+    return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
