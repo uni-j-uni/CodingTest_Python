ec490fdb5362ebc7d977cf9c25482f43bc11fed7
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-10-13 14:32:07 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
     answer = [0, 0]
     for num in num_list:
-        if num % 2 == 0:
-            answer[0] += 1
-        else:
-            answer[1] += 1
+        answer[num % 2] += 1
     return answer
