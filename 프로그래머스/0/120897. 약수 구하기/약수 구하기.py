71493fa091f9f68688872641a9843c53764a24c3
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-10-16 13:14:08 +0900

    [level 0] Title: 약수 구하기, Time: 0.92 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,2 @@
 def solution(n):
-    answer = []
-    for i in range(n):
-        if n % (i + 1) == 0:
-            answer.append(i + 1)
-    return answer
+    return [i for i in range(1, n + 1) if n % i == 0]
