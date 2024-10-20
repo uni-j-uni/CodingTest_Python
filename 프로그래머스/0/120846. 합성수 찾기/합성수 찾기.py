aaca74e466765832de235baaf0e9338465e93057
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-10-20 20:22:03 +0900

    [level 0] Title: 합성수 찾기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,11 +1,16 @@
+2
+3
+4
+5
+6
+7
+8
+9
 def solution(n):
     answer = 0
-
-    for i in range(1, n+1):
-        number = 0
-        for j in range(1, i+1):
-            if i//j==i/j:
-                number+=1
-        if number>=3:
-            answer+=1
+    for i in range(4, n + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                answer += 1
+                break
     return answer
