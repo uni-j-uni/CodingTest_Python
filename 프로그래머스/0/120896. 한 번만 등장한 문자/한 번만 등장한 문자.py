72db87edb4a3158aef670af4d89913c9626f8ee1
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-10-23 06:24:29 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,13 +1,2 @@
 def solution(s):
-    answer = ''
-    i = 0
-    while(True):
-        if s.count(s[i]) > 1:
-            s = s.replace(s[i], '')
-        else:
-            answer += s[i]
-            if i + 1 >= len(s):
-                break
-            else:
-                i += 1
-    return ''.join(sorted(answer))
+    return ''.join(sorted(ch for ch in s if s.count(ch) == 1))
