4e286b1dd438f3499511162a1a78883059915f45
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-10-27 15:32:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 207.59 ms, Memory: 17.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while (i < len(arr)):
+        if not stk: stk.append(arr[i])
+        elif stk[-1] == arr[i]: del stk[-1]
+        else: stk.append(arr[i])
+        i += 1
+    return [-1] if not stk else stk
