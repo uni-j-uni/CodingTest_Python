fc4cb10d856927a3ce18de7e70f1895d3e7a40f8
HeeJun Yoon <unijun0109@gmail.com>
2025-09-24 22:48:52 +0900

    [level 1] Title: 약수의 합, Time: 0.27 ms, Memory: 9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,10 +1,2 @@
-import math
-
 def solution(n):
-    if n == 0:
-        return 0
-    result = 1
-    for i in range(2, n + 1):
-        if n % i == 0:
-            result += i
-    return result
+    return sum([i for i in range(1, n + 1) if n % i == 0])
