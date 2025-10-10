b898db9028e5bcd9cc5821e6c05d21c76f42112d
HeeJun Yoon <unijun0109@gmail.com>
2025-10-11 01:50:05 +0900

    [level 1] Title: 덧칠하기, Time: 4.79 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,11 +1,10 @@
 def solution(n, m, section):
-    answer = 0
-    section.sort()
-    i = 0
-    while i < len(section):
-        start = section[i]
-        end = start + m
-        while i < len(section) and section[i] < end:
-            i += 1
-        answer += 1
+    answer = 1
+    start = section[0]
+    
+    for num in section:
+        if num - start >= m:
+            start = num
+            answer += 1
+
     return answer
